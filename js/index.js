2f4af59f952c83780895386cd866d73227f73341
fengyun <ecustfengyun@163.com>
2018-09-18 21:40:27 +0800

    初稿

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(function () {
+
+    //分页
+    $('#fullpage').fullpage({
+        navigation: true,
+        verticalCentered:false,
+        sectionsColor: ['#84ada2','#108e83','#4b5b8b','#935b4b','#49819c','#9e956e']
+    });
+})
